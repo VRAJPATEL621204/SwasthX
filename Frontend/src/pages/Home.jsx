b2dfc5a29db41e90f1bbd6e8f4770759d6cde47a
VRAJPATEL621204 <vrajpatel621204@gmail.com>
2025-08-07 12:35:08 +0530

    Bug fix 4

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -45,7 +45,7 @@ function Home() {
                 {...(id !== "path6" && { strokeDasharray: "0.3" })}
               />
               <polygon points={`0,-${0.6 * scale} ${1 * scale},0 0,${0.6 * scale}`} fill="#3B82F6">
-                <animateMotion dur="4s" repeatCount="indefinite" rotate="auto" begin="none">
+                <animateMotion dur="4s" repeatCount="indefinite" rotate="auto" begin="0s">
                   <mpath href={`#${id}`} />
                 </animateMotion>
               </polygon>
